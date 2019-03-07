9f076466a49e694b439e73c2e8bbbcc13bc55daa
dkutlesic <dubravka97@gmail.com>
2019-03-07 17:07:34 +0100

    server init

---
+++ server.h
@@ -0,0 +1,23 @@
+#include "subserver.h"
+#include "client.h"
+#include "item.h"
+#include <vector>
+
+using namespace std;
+
+private 
+class Server
+{
+private:
+    vector<Subserver> subservers;
+    int period;
+public:
+ Server(/* args */);
+     Server();
+};
+ Server: Server(/* args */)
+{
+}
+ Server:: Server()
+{
+}
