1e796cdfde6ce2afc46d9edf7bc0aa13f542e1e5
ognjenivuk <ognjen7amg@gmail.com>
2019-03-07 16:39:19 +0100

    adding some structure files

---
+++ client.h
@@ -0,0 +1,13 @@
+#include<string>
+#include<vector>
+
+private class Client{
+private:
+    double money;
+    string username,password;
+    std::vector<Subserver> subservers;
+public:
+    Client() { }
+    ~Client(){}
+    Client
+}
---
+++ item.h
@@ -0,0 +1,10 @@
+#include<string>
+#include<time.h>
+
+
+struct Item
+{
+    double price;
+    string names;
+    tm ending_time;    
+};
