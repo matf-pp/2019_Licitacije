57706ac38547ce150ee5e89b9de5e2969f708962
ognjenivuk <ognjen7amg@gmail.com>
2019-05-02 12:05:49 +0200

    making remote traits

---
+++ Licitecije/src/main/scala/Package/Item.scala
@@ -0,0 +1,12 @@
+package Package
+
+/**
+  * @constructor
+  *
+  * @param price price of the item we are making
+  * @param name name of the item we are making
+  */
+class Item(var price:Double=0,var name:String) extends Thread{
+
+
+}
---
+++ Licitecije/src/main/scala/Package/RemoteClient.scala
@@ -0,0 +1,32 @@
+package Package
+
+import java.rmi.{Remote, RemoteException}
+
+trait RemoteClient extends Remote{
+
+  /**
+    *
+    * @param item what we should buy
+    * @throws
+    * @return tell us if client successfully bought the item
+    */
+  @throws[RemoteException]
+  def buy(item: Item):Boolean
+
+  /**
+    *
+    * @param item what client is selling
+    * @throws
+    */
+  @throws[RemoteException]
+  def sell(item: Item)
+
+  /**
+    * this function provide us with updated items(we want to know their prices)
+    *
+    * @param items items of interest for client
+    * @throws
+    */
+  @throws[RemoteException]
+  def updatePrices(items:List[Item])
+}
